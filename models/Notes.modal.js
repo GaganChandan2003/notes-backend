59451826aadf6cf6eb5364ea843e605299fc8170
GaganChandan2003 <101565932+GaganChandan2003@users.noreply.github.com>
2022-11-24 18:56:02 +0530

    Updated with date

--- models/Notes.modal.js
+++ models/Notes.modal.js
@@ -3,9 +3,10 @@ const mongoose=require("mongoose");
 const notesSchema= new mongoose.Schema({
   title:{type:String,required:true},
   note:{type:String,required:true},
+  date:{type:String,required:true},
   label:{type:String,required:true},
   userId:{type:String,required:true},
-},{ timestamps: true })
+})
 
 
 const NotesModel=mongoose.model("note",notesSchema);
